3040da8831fe748c0eb2744e2799dca6ccaa96f9
Valenron <133826969+Valenron@users.noreply.github.com>
2023-05-16 22:24:25 -0400

    Add files via upload

---
+++ JavaScript/pagina1.js
@@ -0,0 +1,5 @@
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
+function showalert() {alert('¡Funcion de ALERTA desde JavaScript!');}
